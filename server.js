af49dd5f6903004fc168e078f1ae01c0ddec1ba7
S.P. ROY <sprithi44@gmail.com>
2023-03-10 18:28:07 -0500

    express is set up for parsing

--- server.js
+++ server.js
@@ -2,3 +2,12 @@
 const express = require('express');
 const fs = require('fs');
 const uuid = require('uuid');
+
+// Set up the Express app
+const app = express();
+
+// Set up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
