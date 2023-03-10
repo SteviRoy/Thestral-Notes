f68aadbaf0e395a1e53b347878ff1e74cfcff754
S.P. ROY <sprithi44@gmail.com>
2023-03-10 18:33:50 -0500

    port established and handeler for GET added

--- server.js
+++ server.js
@@ -11,3 +11,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// Set up the port number
+const PORT = process.env.PORT || 3001;
+
+// Route handler for the GET /api/notes route
+app.get('/api/notes', (req, res) => {
+  // Read the db.json file
+  const notesData = fs.readFileSync('./db/db.json', 'utf8');
+  // Parse the JSON data
+  const notes = JSON.parse(notesData);
+  // Return the saved notes as JSON
+  res.json(notes);
+});
+
